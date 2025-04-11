67e0fac517c4904afc5e744a17902d65e3b525cc
Alonso-S <alonso.s2k5@gmail.com>
2025-04-10 21:53:26 -0500

    ✨ refactor(app): reorganiza importaciones y mejora estructura de código - Se han movido las importaciones a su ubicación correcta. - Se ha agregado un nuevo import para `format_analysis_response` y se ha modificado el flujo de retorno.

--- static-analysis/app/main.py
+++ static-analysis/app/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from analyzers.bandit_analyzer import analyze_with_bandit
-from analyzers.radon_analyzer import analyze_with_radon
+from app.schemas.analysis import AnalysisRequest
+from app.analyzers.bandit_analyzer import analyze_with_bandit
+from app.analyzers.radon_analyzer import analyze_with_radon
+from app.analyzers.formatter import format_analysis_response
 import os
 
 app = FastAPI()
 
-class AnalysisRequest(BaseModel):
-    code_path: str  # Ruta al directorio o archivo con el código fuente
-    config_file: str = None  # Ruta al archivo de configuración de Bandit (opcional)
-    debug: bool = True  # Modo depuración
-    analysis_types: list = ["bandit", "radon"]  # Análisis a ejecutar: Bandit, Radon
 
 @app.post("/analyze/")
 async def analyze_code(request: AnalysisRequest):
@@ -22,10 +18,9 @@ async def analyze_code(request: AnalysisRequest):
     # Verificar si la ruta es válida
     if not os.path.exists(code_path):
         raise HTTPException(status_code=400, detail="La ruta proporcionada no existe.")
-    
+
     results = {}
 
-    # Analizar con Bandit si está en la lista de análisis
     if "bandit" in request.analysis_types:
         results["bandit"] = analyze_with_bandit(
             code_path=code_path,
@@ -33,8 +28,7 @@ async def analyze_code(request: AnalysisRequest):
             debug=request.debug
         )
 
-    # Analizar con Radon si está en la lista de análisis
     if "radon" in request.analysis_types:
         results["radon"] = analyze_with_radon(code_path=code_path)
 
-    return results
+    return format_analysis_response(results)
