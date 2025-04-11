d42e8098c9b9364aef8e0473a71ea522ebccc82b
Alonso-S <alonso.s2k5@gmail.com>
2025-04-10 21:55:14 -0500

    ✨ feat(app): agregar configuraciones y nuevos endpoints - Se ha agregado la configuración de la aplicación utilizando `settings`. - Se han añadido los endpoints: `/`, `/health` y `/versions`.

--- static-analysis/app/main.py
+++ static-analysis/app/main.py
@@ -3,10 +3,31 @@ from app.schemas.analysis import AnalysisRequest
 from app.analyzers.bandit_analyzer import analyze_with_bandit
 from app.analyzers.radon_analyzer import analyze_with_radon
 from app.analyzers.formatter import format_analysis_response
+from app.config.settings import settings
 import os
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+    debug=settings.DEBUG,
+)
 
+
+@app.get("/")
+def root():
+    return {
+        "message": "Bienvenido a DebtCleaner - Módulo de Análisis Estático",
+        "available_endpoints": ["/analyze/", "/health", "/versions"]
+    }
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/versions")
+def tool_versions():
+    from app.utils.helpers import get_tool_versions
+    return get_tool_versions()
 
 @app.post("/analyze/")
 async def analyze_code(request: AnalysisRequest):
