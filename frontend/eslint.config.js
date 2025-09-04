db4954b3d98853c7cec69fa894cd35b13d081ced
José Santisteban <alonso.s2k5@gmail.com>
2025-09-03 22:18:53 -0500

    chore(frontend): agregar configuración ESLint, prettier y reglas básicas

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
-
+import prettier from 'eslint-config-prettier'
 export default tseslint.config([
   globalIgnores(['dist']),
   {
@@ -20,4 +20,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  prettier,
 ])
