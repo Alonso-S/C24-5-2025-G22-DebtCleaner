37029444ec50f5f96ce43bdb90603794d7e4ed97
José Santisteban <alonso.s2k5@gmail.com>
2025-09-22 18:53:17 -0500

    refactor(backend-web/prisma): ajustar importación de PrismaClient en la configuración

--- backend-web/src/config/prisma.ts
+++ backend-web/src/config/prisma.ts
@@ -1,15 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../generated/prisma";
 
 const globalForPrisma = globalThis as typeof globalThis & {
   prisma: PrismaClient | undefined;
 };
-
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query', 'warn', 'error'], 
+    log: ["query", "warn", "error"],
   });
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
