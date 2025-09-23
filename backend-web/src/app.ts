0d3e6eb3ba9ab948ed49321e5d96fb1ec7220b14
José Santisteban <alonso.s2k5@gmail.com>
2025-09-22 19:13:33 -0500

    feat(backend-web/auth): agregar enrutador para gestionar solicitudes de códigos de inicio de sesión

--- backend-web/src/app.ts
+++ backend-web/src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { errorHandler } from "./common/middleware/errorHandler";
-
+import { authRouter } from "./modules/auth";
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRouter);
 app.use(errorHandler);
 
 export default app;
